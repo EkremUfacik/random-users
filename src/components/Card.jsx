72842533dcbe8910cd2efc6bc158a0e82bf31d3f
ekrem <ekremufacik07@gmail.com>
2022-10-26 10:37:37 +0300

    ss

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,7 @@ import {
   FcBusinessContact,
   FcBookmark,
 } from "react-icons/fc";
+import { useEffect, useState } from "react";
 
 const Card = (person) => {
   const {
@@ -17,12 +18,26 @@ const Card = (person) => {
     dob,
   } = person;
 
-  const showInfo = (text, info) => {
-    return (
-      (document.getElementById("p1").innerText = `My ${text} is`),
-      (document.getElementById("p2").innerText = info)
-    );
-  };
+  const [text, setText] = useState({});
+
+  const { content, desc } = text;
+
+  useEffect(() => {
+    setText({
+      content: [first + " " + last],
+      desc: "name",
+    });
+    // eslint-disable-next-line
+  }, [person]);
+
+  // const showInfo = (text, info) => {
+  //   return (
+  //     (document.getElementById("p1").innerText = `My ${text} is`),
+  //     (document.getElementById("p2").innerText = info)
+  //   );
+  // };
+
+  console.log(text);
 
   return (
     <div className="info text-center pb-3">
@@ -32,34 +47,48 @@ const Card = (person) => {
 
       <div className="mt-3">
         <p id="p1" className="">
-          {first}
+          My {desc || "name"} is
         </p>
         <p id="p2" className="">
-          {last}
+          {content || [first + " " + last]}
         </p>
       </div>
 
       <div className="d-flex gap-4 justify-content-evenly">
         <div className="logo">
           <FcBusinessContact
-            onMouseOver={() => showInfo("name", `${first} ${last}`)}
-          />
-        </div>
-        <div className="logo">
-          <FcAddressBook onMouseOver={() => showInfo("email", email)} />
-        </div>
-        <div className="logo">
-          <FcPhoneAndroid onMouseOver={() => showInfo("phone", phone)} />
-        </div>
-        <div className="logo">
-          <FcHome
             onMouseOver={() =>
-              showInfo("adress", location.city + "/" + location.country)
+              setText({
+                content: [first + " " + last],
+                desc: "name",
+              })
             }
           />
         </div>
         <div className="logo">
-          <FcBookmark onMouseOver={() => showInfo("age", dob.age)} />
+          <FcAddressBook
+            onMouseOver={() => setText({ content: email, desc: "email" })}
+          />
+        </div>
+        <div className="logo">
+          <FcPhoneAndroid
+            onMouseOver={() => setText({ content: phone, desc: "phone" })}
+          />
+        </div>
+        <div className="logo">
+          <FcHome
+            onMouseOver={() =>
+              setText({
+                content: [location.city + " / " + location.country],
+                desc: "address",
+              })
+            }
+          />
+        </div>
+        <div className="logo">
+          <FcBookmark
+            onMouseOver={() => setText({ content: dob.age, desc: "age" })}
+          />
         </div>
       </div>
     </div>
